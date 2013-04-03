ced87007aaef957e5bb0d0b60086d469ff567464
Andreas Lien Olsen <andreas.olsen@gmail.com>
2013-04-03 19:46:06 +0200

    handle numbers below 10

---
+++ roman/RomanNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roman
+{
+    public class RomanNumbers
+    {
+        private static Dictionary<int, string> _lookup = new Dictionary<int, string>
+            {
+                //{1, "I"},
+                //{5, "V"},
+                //{10, "X"},
+                {50, "L"},
+                {100, "C"},
+                {500, "D"},
+                {1000, "M"},
+            };
+
+        private static Dictionary<int, string> _lookup2 = new Dictionary<int, string>
+            {
+                {1, "I"},
+                {2, "II"},
+                {3, "III"},
+                {4, "IV"},
+                {5, "V"},
+                {6, "VI"},
+                {7, "VII"},
+                {8, "VIII"},
+                {9, "IX"},
+                {10, "X"},
+            };
+
+        public static string ToRoman(int number)
+        {
+            if (_lookup.ContainsKey(number))
+                return _lookup[number];
+            if (_lookup2.ContainsKey(number))
+                return _lookup2[number];
+
+            return "";
+        }
+    }
+}
---
+++ roman/RomanTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace roman
+{
+    [TestFixture]
+    public class RomanTests
+    {
+        [TestCase(1, "I")]
+        [TestCase(5, "V")]
+        [TestCase(10, "X")]
+        [TestCase(50, "L")]
+        [TestCase(100, "C")]
+        [TestCase(500, "D")]
+        [TestCase(1000, "M")]
+        public void Should_handle_single_letter_numbers(int number, string expected)
+        {
+            RomanNumbers.ToRoman(number).ShouldEqual(expected);
+        }
+        
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        public void Should_handle_numbers_below_10(int number, string expected)
+        {
+            RomanNumbers.ToRoman(number).ShouldEqual(expected);
+        }
+        
+        [TestCase(11, "XI")]
+        public void Should_handle_numbers_below_100(int number, string expected)
+        {
+            RomanNumbers.ToRoman(number).ShouldEqual(expected);
+        }
+        
+        
+    }
+
+}
